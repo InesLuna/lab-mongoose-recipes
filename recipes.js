7d423720759a9a8457172c559d2248ac5e1bc139
ines luna <inesluna19@gmail.com>
2019-10-18 14:20:57 +0200

    done

--- recipes.js
+++ recipes.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const Schema   = mongoose.Schema;
+const Recipe = require("./models/Recipe.js")
 const data = require('./data.js');
 
 mongoose.connect('mongodb://localhost/recipeApp')
@@ -8,3 +8,31 @@ mongoose.connect('mongodb://localhost/recipeApp')
   }).catch(err => {
     console.error('Error connecting to mongo', err);
   });
+
+// introducir a mano una receta
+  let createRecipe = () => {
+    Recipe.create({ title: 'nada', level:"Easy Peasy", cuisine: 'nada' })
+      .then(recipe => { console.log('The user is saved and its value is: ', recipe) })
+      .catch(err => { console.log('An error happened:', err)
+  })};
+
+//introducir varias recetas
+
+let insertRecipes = (arr)=> {
+  Recipe.insertMany(arr)
+    .then(recipe => { console.log('The user is saved and its value is: ', recipe) })
+    .catch(err => { console.log('An error happened:', err)})}
+
+let updateByTitle = (recipeTitle) =>{
+  Recipe.updateOne({title: recipeTitle}, {duration: 100})
+    .then(recipe => console.log(`${recipe} has been updated`))
+    .catch(err => { console.log('An error happened:', err)})
+}
+
+let removeRecipe = (recipeTitle) =>{
+  Recipe.deleteOne({title: recipeTitle})
+    .then(recipe => console.log(`The recipie has been deleted`))
+    .catch(err => { console.log('An error happened:', err)})
+}
+
+removeRecipe('Carrot Cake')
